0658e0f9e1b8fb440a25e162644e30fedd3d4965
Matías Julián Apablaza <mapablaza@itba.edu.ar>
2020-11-08 20:03:21 -0300

    Finalice el delete y agregue restricciones unique a la creacion de esquema

--- sqlscript.sql
+++ sqlscript.sql
@@ -1,32 +1,35 @@
 ------------------------------------ SCHEMA CREATION ------------------------------------
---
--- CREATE TABLE pais(
---   	id_pais SERIAL NOT NULL PRIMARY KEY,
---   	pais TEXT NOT NULL
--- );
---
--- CREATE TABLE provincia(
---   	provincia INT NOT NULL PRIMARY KEY ,
---   	id_pais INT NOT NULL,
---   	FOREIGN KEY (id_pais) REFERENCES pais ON DELETE RESTRICT
--- );
---
--- CREATE TABLE departamento(
---   	id_departamento SERIAL NOT NULL PRIMARY KEY,
---   	departamento TEXT NOT NULL,
---   	provincia INT NOT NULL,
---   	FOREIGN KEY (provincia) REFERENCES provincia ON DELETE RESTRICT
--- );
---
--- CREATE TABLE localidad (
---   	id_localidad SERIAL NOT NULL PRIMARY KEY,
---   	nombre TEXT NOT NULL,
---   	canthab INT,
---   	id_departamento INT NOT NULL,
---   	FOREIGN KEY (id_departamento) REFERENCES departamento ON DELETE RESTRICT
---  );
---
--- ------------------------------------------- PSM/TRIGGERS -------------------------------------------
+
+CREATE TABLE pais(
+  	id_pais SERIAL NOT NULL PRIMARY KEY,
+  	pais TEXT NOT NULL,
+  	UNIQUE (pais)
+);
+
+CREATE TABLE provincia(
+  	provincia INT NOT NULL PRIMARY KEY ,
+  	id_pais INT NOT NULL,
+  	FOREIGN KEY (id_pais) REFERENCES pais ON DELETE RESTRICT
+);
+
+CREATE TABLE departamento(
+  	id_departamento SERIAL NOT NULL PRIMARY KEY,
+  	departamento TEXT NOT NULL,
+  	provincia INT NOT NULL,
+  	FOREIGN KEY (provincia) REFERENCES provincia ON DELETE RESTRICT,
+  	UNIQUE (departamento, provincia)
+);
+
+CREATE TABLE localidad (
+  	id_localidad SERIAL NOT NULL PRIMARY KEY,
+  	nombre TEXT NOT NULL,
+  	canthab INT,
+  	id_departamento INT NOT NULL,
+  	FOREIGN KEY (id_departamento) REFERENCES departamento ON DELETE RESTRICT,
+  	UNIQUE (nombre, id_departamento)
+ );
+
+------------------------------------------- DATA IMPORT -------------------------------------------
 
 CREATE OR REPLACE FUNCTION insert_csv_row() RETURNS trigger AS $$
 DECLARE
@@ -58,32 +61,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION delete_psm() RETURNS trigger AS $$
-DECLARE
-  pais_id int;
-  departamento_id int;
-BEGIN
-
-  SELECT id_departamento FROM departamento WHERE (departamento.departamento=old.departamento AND departamento.provincia=old.provincia)INTO departamento_id;
-  delete from localidad where id_departamento = departamento_id;
-  IF NOT EXISTS(SELECT * FROM aux WHERE  departamento= old.departamento) THEN
-    delete from departamento where id_departamento = departamento_id ;
-  END IF;
-    
-   
-    RETURN null;
-END;
-$$ LANGUAGE plpgsql;
-
-
-
-CREATE TRIGGER delete_handler after delete ON aux
-FOR EACH ROW 
-EXECUTE PROCEDURE delete_psm();
-
-
-------------------------------------------- DATA IMPORT -------------------------------------------
-
 CREATE VIEW csv_view AS(
     SELECT localidad.nombre,
            pais.pais,
@@ -101,6 +78,36 @@ FOR EACH ROW EXECUTE PROCEDURE insert_csv_row();
 
 COPY csv_view FROM 'C:\Users\matig\Desktop\localidades.csv' (FORMAT CSV, HEADER);
 
+------------------------------------------- DATA DELETE -------------------------------------------
+
+CREATE OR REPLACE FUNCTION delete_csv_row() RETURNS trigger AS $$
+DECLARE
+    locId int; depId int; paisId int;
+BEGIN
+
+    SELECT id_pais FROM pais WHERE pais.pais=old.pais INTO paisId;
+    SELECT id_departamento FROM departamento WHERE departamento.departamento=old.departamento AND provincia=old.provincia INTO depId;
+    SELECT id_localidad FROM localidad WHERE localidad.nombre=old.nombre AND localidad.id_departamento=depId INTO locId;
+
+    DELETE FROM localidad WHERE localidad.id_localidad=locId;
+
+    IF NOT EXISTS(SELECT * FROM localidad WHERE localidad.id_departamento = depId) THEN
+        DELETE FROM departamento WHERE departamento.id_departamento = depId;
+    END IF;
+
+    IF NOT EXISTS(SELECT * FROM departamento WHERE departamento.provincia = old.provincia) THEN
+        DELETE FROM provincia WHERE provincia.provincia = old.provincia;
+    END IF;
+
+    IF NOT EXISTS(SELECT * FROM provincia WHERE provincia.id_pais = paisId) THEN
+        DELETE FROM pais WHERE pais.id_pais = paisId;
+    END IF;
+
+    RETURN NULL;
+
+END;
+$$ LANGUAGE plpgsql;
 
 
-
+CREATE TRIGGER delete_csv_row INSTEAD OF DELETE ON csv_view
+FOR EACH ROW EXECUTE PROCEDURE delete_csv_row();
